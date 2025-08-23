8f13e029b7edb0912051579c7cdddf043c67442c
sunnyday1973 <gannaua@gmail.com>
2025-08-23 18:58:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,6 @@ let amigos = []
 
 function agregarAmigo() {
     let nombreIngresado = document.querySelector('amigo').value
-}
-
-function sortearAmigo() {
-
-}
 /*
 listaAmigos
 resultado
@@ -17,6 +12,18 @@ sortearAmigo
 Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
 Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push().
 Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía.
+*/}
+
+function sortearAmigo() {
+/*
+Crea una función que recorra el array amigos y agregue cada nombre como un elemento <li> dentro de una lista HTML. Usa innerHTML para limpiar la lista antes de agregar nuevos elementos.
+Tareas específicas:
+Obtener el elemento de la lista: Utilizar document.getElementById() o document.querySelector() para seleccionar la lista donde se mostrarán los amigos.
+Limpiar la lista existente: Establecer lista.innerHTML = "" para asegurarse de que no haya duplicados al actualizar.
+Iterar sobre el arreglo: Usa un bucle for para recorrer el arreglo amigos y crear elementos de lista (<li>) para cada título.
+Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.
 */
+}
+
 
 
