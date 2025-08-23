479a4f9a06f8449acbdc4df8bca36b1ae4a4b59d
sunnyday1973 <gannaua@gmail.com>
2025-08-23 18:47:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+
+function agregarAmigo() {
+    let nombreIngresado = document.querySelector('amigo').value
+}
+
+function sortearAmigo() {
+
+}
+/*
+listaAmigos
+resultado
+button-add
+button-sortear-amigo
+Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
+Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push().
+Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía.
+*/
