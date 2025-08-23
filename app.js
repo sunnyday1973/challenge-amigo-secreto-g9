646d165a33e90029015750bdea3ab6aa1d2ebf1e
sunnyday1973 <gannaua@gmail.com>
2025-08-23 18:55:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,10 +12,11 @@ function sortearAmigo() {
 /*
 listaAmigos
 resultado
-addAmigo
+agregarAmigo
 sortearAmigo
 Validar la entrada: Implementar una validación para asegurarse de que el campo no esté vacío. Si está vacío, mostrar un alert con un mensaje de error: "Por favor, inserte un nombre."
 Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push().
 Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía.
 */
+
 
