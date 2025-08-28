6e4e2e8c43cbe18b87078f53587eec492d9dc59a
sunnyday1973 <gannaua@gmail.com>
2025-08-28 22:08:51 +0000

    se agregan los estados del juego

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let amigos = []
 let amigosSorteados = []
+let sorteoInicio = false
+let sorteoFinalizo = false
 const botonAgregar = document.querySelector('#agregarAmigo')
 const inputNombreAmigo = document.querySelector('#amigo')
 const botonSortear = document.querySelector('#sortearAmigo')
@@ -8,42 +10,48 @@ const botonSortear = document.querySelector('#sortearAmigo')
 function agregarAmigo() {
     let nombreAmigo = inputNombreAmigo.value.trim()
 
+    if(sorteoInicio || sorteoFinalizo) {
+        alert(`El sorteo ya ha ${sorteoInicio ? 'iniciado':'finalizado'}. Renicia el juego, para poder agregar mas integrantes.`)
+        return false
+    }
+
     if(nombreAmigo === '') {
         console.log('nombre vacio')
         alert('Por favor, inserte un nombre.')
-        return 
+        return false
     }else {
         nombreAmigo = String(nombreAmigo).toLocaleUpperCase();
+
         if(amigos.includes(nombreAmigo)) {
             console.log('ingrese otro')
             alert(`Nombre ${nombreAmigo} ya esta en la lista. Ingrese otro nombre.`)
-            return
+            return false
         } else {
             amigos.push(nombreAmigo)   
             inputNombreAmigo.value = ''
-            actualizarLista('listaAmigos',amigos)
+            actualizarLista('listaAmigos',amigos, sorteoInicio)
             if(amigos.length >= 2) {
-                botonSortear.removeAttribute('disabled')
                 botonSortear.setAttribute('class', 'button-draw')                
             }
         }
     }
-    return
+
+    return true
 }
 
-function actualizarLista(idLista, amigos, sorteado=false) {
+function actualizarLista(idLista, amigos, sorteoInicio=false) {
     let htmlAmigos = document.querySelector(`#${idLista}`)
     htmlAmigos.innerHTML = ''
     let lista = ''
     let marcado = ''
-    
+
     for (let i = 0; i < amigos.length; i++) {
-        marcado = (sorteado && amigosSorteados.includes(i)) ? '<img src="assets/check.png" alt="Ícono para marcardo">':''
+        marcado = (sorteoInicio && amigosSorteados.includes(i)) ? '<img src="assets/check.png" alt="Ícono para marcardo">':''
         lista += `<li>${marcado}${amigos[i]}</li>`;
     }
     
     htmlAmigos.innerHTML = lista
-    return
+    return true
 }
 
 function sortearAmigo() {
@@ -59,29 +67,33 @@ function sortearAmigo() {
         return false
     }
     
-    botonAgregar.setAttribute('disabled', 'disabled')
+    sorteoInicio = true
     botonAgregar.setAttribute('class', 'button-add-disabled')
     let numeroSorteado = Math.floor(Math.random()*amigos.length)
     resultado.innerHTML = `El nombre del amigo secreto es ${amigos[numeroSorteado]}<br/>`
     amigosSorteados.push(numeroSorteado)
-    actualizarLista('listaAmigos',amigos,true)
-    
-    if ((amigos.length - amigosSorteados.length) < 2) {
-        console.log('quedo 1')
-        botonSortear.setAttribute('disabled', 'disabled');
+    actualizarLista('listaAmigos',amigos,sorteoInicio)
+
+    if(amigos.length == amigosSorteados.length) {
+        alert('Se han sorteado todos los amigos. Reinicia el juego, si desea hacer otro sorteo.')
         botonSortear.setAttribute('class', 'button-draw-disabled')
+        sorteoFinalizo = true
+        return false
     }
+
+    return true
 }
 
 function resetearJuego() {
     botonAgregar.removeAttribute('disabled')
     botonAgregar.setAttribute('class', 'button-add')
-    botonSortear.setAttribute('disabled', 'disabled')
     botonSortear.setAttribute('class', 'button-draw-disabled')
     document.querySelector('#listaAmigos').innerHTML = ''
     document.querySelector('#resultado').innerHTML = ''
-
+    sorteoInicio = false
+    sorteoFinalizo = false
     amigos = []
+    amigosSorteados = []
 
-    return
+    return true
 }
